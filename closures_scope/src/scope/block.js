f16fd790039863791a31a993e67681fddfe06f0b
andresdev <andresmestra11a@gmail.com>
2021-08-27 22:51:29 -0500

    scope local en bloques

---
+++ closures_scope/src/scope/block.js
@@ -0,0 +1,12 @@
+const fruits = () => {
+  if (true) {
+    var fruits1 = "apple";
+    let fruits2 = "banana";
+    const fruits3 = "kiwi";
+
+    console.log({ fruits2, fruits3 });
+  }
+
+  console.log({ fruits1, fruits2, fruits3 });
+};
+fruits();
